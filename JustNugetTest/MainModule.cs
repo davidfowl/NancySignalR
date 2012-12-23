4321b41c47e7105d87ef80e0034cd60d2c1f86d5
Louis DeJardin <louis.dejardin@gmail.com>
2012-12-22 23:46:06 -0800

    Trying an autostartup example

---
+++ JustNugetTest/MainModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace JustNugetTest
+{
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = _ => "Hello, Nancy!";
+        }
+    }
+}
---
+++ Nancy.Hosting.Owin.Shim/NancyAutoStartup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Nancy.Hosting.Owin;
+using Owin;
+
+[assembly: OwinAutoStartup(Type = typeof(NancyAutoStartup), MethodName = "Configuration")]
+
+internal class OwinAutoStartupAttribute : Attribute
+{
+    public Type Type { get; set; }
+    public string MethodName { get; set; }
+}
+
+namespace Nancy.Hosting.Owin
+{
+    public class NancyAutoStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            if (SetNancyFlag(app))
+            {
+                app.UseNancy();
+            }
+        }
+
+        internal static bool SetNancyFlag(IAppBuilder app)
+        {
+            object value;
+            if (app.Properties.TryGetValue("nancy.Registered", out value))
+            {
+                return false;
+            }
+
+            app.Properties["nancy.Registered"] = null;
+            return true;
+        }
+    }
+}
